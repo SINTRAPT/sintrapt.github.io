482bfed64f59777737e574ad9a8ea5f805491ef4
SINTRAPT <alvarojpduarte@gmail.com>
2026-02-16 01:24:30 -0300

    Update lp.js

--- lp/fera/js/lp.js
+++ lp/fera/js/lp.js
@@ -1,1 +1,21 @@
+<script>
+  window.dataLayer = window.dataLayer || [];
 
+  // Evento: LP view
+  window.dataLayer.push({
+    event: "lp_view",
+    product_slug: "fera"
+  });
+
+  // Evento: clique no RCA
+  document.addEventListener("click", function (e) {
+    const btn = e.target.closest("[data-rca]");
+    if (!btn) return;
+
+    window.dataLayer.push({
+      event: "rca_click",
+      product_slug: "fera",
+      cta_id: btn.getAttribute("data-cta-id") || "cta_main"
+    });
+  });
+</script>
