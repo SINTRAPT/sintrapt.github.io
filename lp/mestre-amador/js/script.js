790e5b582088f31673f961ac535164217e6da8b9
SINTRAPT <alvarojpduarte@gmail.com>
2026-03-24 18:38:36 -0300

    Refactor click event listener for better readability

--- lp/mestre-amador/js/script.js
+++ lp/mestre-amador/js/script.js
@@ -1,30 +1,30 @@
 window.product_name = "mestre-amador";
 window.lp_version = "v1";
 
-document.addEventListener("click", function(e){
+document.addEventListener("click", function(e) {
 
-var el = e.target.closest(".cta_tracking");
-if(!el) return;
+    var el = e.target.closest(".cta_tracking");
+    if (!el) return;
 
-var eventName = el.dataset.event || "rca_click";
-var location = el.dataset.location || "unknown";
-var type = el.dataset.type || "unknown";
-var url = el.href || "";
+    var eventName = el.dataset.event || "rca_click";
+    var location = el.dataset.location || "unknown";
+    var type = el.dataset.type || "unknown";
+    var url = el.href || "";
 
 
-dataLayer.push({
-event: eventName,
-button_location: location,
-button_type: type,
-button_url: url,
-});
+    dataLayer.push({
+        event: eventName,
+        button_location: location,
+        button_type: type,
+        button_url: url,
+    });
 
-if(type === "checkout" || type === "pagina_vendas"){
-e.preventDefault();
+    if (type === "checkout" || type === "pagina_vendas") {
+        e.preventDefault();
 
-setTimeout(function(){
-window.open(url, "_blank");
-},150);
-}
+        setTimeout(function() {
+            window.open(url, "_blank");
+        }, 150);
+    }
 
 });
